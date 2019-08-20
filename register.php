e033d857ec9d13150fdd6ce4e92c5431850976e9
nsbano <50585967+nsbano@users.noreply.github.com>
2019-08-20 10:38:05 -0300

    Add files via upload

--- register.php
+++ register.php
@@ -1,4 +1,23 @@
-<html>
+<?php
+  $pageTitle = 'Formulario de Registro | Proyectos DH';
+  require_once 'forms/forms.php';
+  if ($_POST) {
+
+    if (length(old('nombre'), 5, 30)) {
+      addError('nombre', 'El nombre debe tener entre 5 y 30 caracteres');
+    }
+
+    if (!email(old('email'))) {
+      addError('email', 'Debes escribir un email válido');
+    }
+    if (isValid()) {
+      header('location: registro-exitoso.php');
+    }
+  }
+?>
+<!DOCTYPE html>
+
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,67 +30,67 @@
 </head>
     <body>
         <?php   require_once 'partials/header.php' ?>
-    <main>
-        <div class="container">
-        <h1>Registrarse</h1>
-        <form class="form-horizontal" role="form">
-            <div class="form-group">
-                <label for="name1" class="col-sm-2 control-label"><b>Nombre:</b></label>
-                <div class="col-sm-4">
-                    <input type="email" class="form-control inputstl" id="name1" placeholder="Nombre Completo">
+        <main>
+            <div class="container">
+            <h1>Registrarse</h1>
+            <form class="form-horizontal" role="form">
+                <div class="form-group">
+                    <label for="name1" class="col-sm-2 control-label"><b>Nombre:</b></label>
+                        <div class="col-sm-6">
+                        <input type="name" class="form-control inputstl" id="name1" name="nombre" placeholder="Nombre Completo" value="<?= old('projectName') ?>">
+                        </div>
+                        <?php if (hasError('nombre')) : ?>
+                    <p class="errors"><?= getError('nombre') ?></p>
+                  <?php endif ?>
+                    </div>
+
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="gender1" class="col-sm-2 control-label"><b>Sexo:</b></label>
-                <div class="col-sm-2">
-                    <select class="form-control inputstl" id="gender1">
-                        <option>Hombre</option>
-                        <option>Mujer</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="email1" class="col-sm-2 control-label"><b>Email:</b></label>
-                <div class="col-sm-5">
-                    <input type="email" class="form-control inputstl" id="email1" placeholder="Email">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="phone1" class="col-sm-2 control-label"><b>Telefono:</b></label>
-                <div class="col-sm-5">
-                    <input type="phone1" class="form-control inputstl" id="phone1" placeholder="Telefono">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="password1" class="col-sm-2 control-label"><b>Contraseña:</b></label>
-                <div class="col-sm-3">
-                    <input type="password1" class="form-control inputstl" id="password1" placeholder="Contraseña">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="password2" class="col-sm-2 control-label"><b>Contraseña:</b></label>
-                <div class="col-sm-5">
-                    <input type="password2" class="form-control inputstl" id="password2" placeholder="Repetir Contraseña">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" class="inputstl"> <b>Recordame</b>
-                        </label>
+                <div class="form-group">
+                    <label for="gender1" class="col-sm-2 control-label"><b>Sexo:</b></label>
+                    <div class="col-sm-6">
+                        <select class="form-control inputstl" id="gender1">
+                            <option>Hombre</option>
+                            <option>Mujer</option>
+                        </select>
                     </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-4">
-                    <button type="submit" class="btn btn-lg btn-block btn-success">Crear Cuenta</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    </main>
-</body>
-  <?php   require_once 'partials/footer.php' ?>
+                <div class="form-group">
+                    <label for="email1" class="col-sm-2 control-label"><b>Email:</b></label>
+                    <div class="col-sm-6">
+                        <input type="email" class="form-control inputstl" id="email1" name="email" placeholder="Email" value="<?= old('email') ?>">
+                        <?php if (hasError('email')) : ?>
+                    <p class="errors"><?= getError('email') ?></p>
+                  <?php endif ?>
+                    </div>
 
+                </div>
+                <div class="form-group">
+                    <label for="user" class="col-sm-2 control-label"><b>Usuario:</b></label>
+                    <div class="col-sm-6">
+                        <input type="user" class="form-control inputstl" id="phone1" name="usuario" placeholder="Usuario">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password1" class="col-sm-2 control-label"><b>Contraseña:</b></label>
+                    <div class="col-sm-6">
+                        <input type="password1" class="form-control inputstl" id="password1" name="password1" placeholder="Contraseña" value="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password2" class="col-sm-2 control-label"><b>Contraseña:</b></label>
+                    <div class="col-sm-6">
+                        <input type="password2" class="form-control inputstl" id="password2" name="repass" placeholder="Repetir Contraseña" value="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-3">
+                        <button type="submit" class="btn btn-lg btn-block btn-success">Crear Cuenta</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div align='center'>Si ya tenes una cuenta clica <a href="sing-in.php" style="color:blue;">aquí</a></br>.</div>
+        </main>
+  <?php   require_once 'partials/footer.php' ?>
+</body>
 </html>
