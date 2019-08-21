4d9bef18e5eddfa14769a72e8d0d85454c9cd90e
gurdanet <50548919+gurdanet@users.noreply.github.com>
2019-08-21 20:42:22 -0300

    Add files via upload

--- register.php
+++ register.php
@@ -16,17 +16,15 @@
   }
 ?>
 <!DOCTYPE html>
-
-
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="style/stylesregister.css">
-        <title>Register | SportShoes</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/stylesregister.css">
+    <title>Register | SportShoes</title>
 </head>
     <body>
         <?php   require_once 'partials/header.php' ?>
@@ -36,15 +34,14 @@
             <form class="form-horizontal" role="form">
                 <div class="form-group">
                     <label for="name1" class="col-sm-2 control-label"><b>Nombre:</b></label>
-                        <div class="col-sm-6">
-                        <input type="name" class="form-control inputstl" id="name1" name="nombre" placeholder="Nombre Completo" value="<?= old('projectName') ?>">
-                        </div>
-                        <?php if (hasError('nombre')) : ?>
-                    <p class="errors"><?= getError('nombre') ?></p>
-                  <?php endif ?>
+                    <div class="col-sm-6">
+                      <input type="name" class="form-control inputstl" id="name1" name="nombre" placeholder="Nombre Completo" value="<?= old('projectName') ?>">
                     </div>
-
+                    <?php if (hasError('nombre')) : ?>
+                      <p class="errors"><?= getError('nombre') ?></p>
+                    <?php endif ?>
                 </div>
+
                 <div class="form-group">
                     <label for="gender1" class="col-sm-2 control-label"><b>Sexo:</b></label>
                     <div class="col-sm-6">
--- sing-in.php
+++ sing-in.php
@@ -2,9 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="singin.css">
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/index.css">
 
     <title>Signin Template for Bootstrap</title>
 
@@ -12,10 +14,10 @@
     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
+  <link rel="stylesheet" href="style/signin.css">
   </head>
+  <body>
   <?php   require_once 'partials/header.php' ?>
-      <body class="text-center">
     <form class="form-signin">
       <img class="mb-4" src="logo.jpg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
