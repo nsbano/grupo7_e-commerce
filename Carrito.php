090149c8aa36e3d18e533999a4ce7e676bc8fe5b
lulimartin <50600060+lulimartin@users.noreply.github.com>
2019-08-27 08:37:09 -0300

    Update Carrito.php

--- Carrito.php
+++ Carrito.php
@@ -15,36 +15,71 @@
 <?php require_once 'partials/header.php' ?>
 
 <body>
+  <div class="container-fluid">
+      <div class="col col-md-9 m-auto">
+        <div class="row">
+          <div class="w-100">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mt-2">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+               
+                <li class="breadcrumb-item active" aria-current="page">Carrito</li>
+              </ol>
+            </nav>
+          </div>
+        </div>
 
-  <main>
-
-  <div class="Guardados">
-  <a class="Guardados" >Carrito (0)</a>
-  </div>
-
-  <div class="Guardados">
-  <a class="Guardados">Guardados (0)</a>
-  </div>
-
-    <div>
-    <h3 class="">Tu carrito está vacío</h3>
-    <p class="">¿No sabés qué comprar?</p>
-  </div>
-
-  <h2 class="">Productos que te interesaron</h2>
-
-  <img class="" src="img/zapatillas/zapatillas_2.jpg">
-<br>
-    $ 1.690
-<div class="">
-
-
-    <img src="img/zapatillas/zapatillas_1.jpg">
-    <br>                                                          ,
-    $ 6900
+        <!-- Carrito -->
+        <div class="row">
+          <div class="col-md-8 offset-md-2 mb-2 bg-white border rounded p-3">
+            <h4 class="d-flex justify-content-between align-items-center mb-3 mw-500">
+              <span class="text-muted">Carrito</span>
+              <span class="badge badge-secondary badge-pill">2</span>
+            </h4>
+            <ul class="list-group mb-3">
+              <li class="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                  <h6 class="my-0">Producto</h6>
+                  <small class="text-muted">Pequeña descripcion del producto</small>
+                </div>
+                <span class="text-muted">$300</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                  <h6 class="my-0">Producto</h6>
+                  <small class="text-muted">Pequeña descripcion del producto</small>
+                </div>
+                <span class="text-muted">$3000</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between bg-light">
+                <div class="text-success">
+                  <h6 class="my-0">Codigo de descuento</h6>
+                  <small>Codigo de ejemplo</small>
+                </div>
+                <span class="text-success">-$2000</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between">
+                <span>Total</span>
+                <strong>$400</strong>
+              </li>
+            </ul>
+            <form>
+              <div class="card p-2">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Codigo de descuento">
+                  <div class="input-group-append">
+                    <button type="submit" class="btn btn-secondary">Ingresar</button>
+                  </div>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary btn-lg btn-block mt-3" name="button">Finalizar compra</button>
+              <p class="text-center mt-2 "><a href="listado.php" class="text-secondary">Seguir comprando</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
 
-	</main>
   <?php require_once 'partials/footer.php' ?>
   </body>
 
